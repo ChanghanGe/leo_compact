98828f7e32d276c3452b637d52720e2a0b61ded7
ChanghanGe <51274778+ChanghanGe@users.noreply.github.com>
2022-12-11 16:39:28 -0600

    Update leo_satellites.py

--- main_program/leo_satellites.py
+++ main_program/leo_satellites.py
@@ -803,9 +803,9 @@ def find_valid_ground_station(hrs, SIMULATION_RANGE, epoch = EPOCH, num_gs = 10,
                         if cur_sat.alt >= ephem.degrees('40'):
                             temp_visible_sats.append(sat)
 
-                    print('here')
                     if len(temp_visible_sats) == 0:
                         print('No Overlapping Visible Satellite')
+                        valid == False:
                         break
 
                 if valid == False:
