fa436643b53fffc89e0a5fef458728e33868b09b
ChanghanGe <51274778+ChanghanGe@users.noreply.github.com>
2022-12-12 14:51:46 -0600

    Update test_mu.py

--- main_program/test_mu.py
+++ main_program/test_mu.py
@@ -166,7 +166,7 @@ for t in range(24):
                 az[key] = lon
 
             data = pd.DataFrame.from_dict(results)
-            print(os.system('pwd'))
+            os.system('pwd')
             data.to_csv('./output/rss_' + city + '_user_' + str(gs) + '_' + BD + '.csv')  
             # data = pd.DataFrame.from_dict(vis)
             # data.to_csv('visibility_' + citys[city] + '_' + BD + '.csv')  
